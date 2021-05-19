2bfb00fe27fc384d1ff7d0e8160e1edad5b03e10
Danielp <64538910+Danielpovolni@users.noreply.github.com>
2021-05-19 22:49:27 +0200

    Update one_line.cpp

--- one_line.cpp
+++ one_line.cpp
@@ -1,1 +1,25 @@
-
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    int numbers, sum;
+    string operation;
+    cin >> operation;
+    if(operation == "+"){
+        while(cin >> numbers){
+            sum = numbers + sum;
+        }}
+    if(operation == "-"){
+        while(cin >> numbers){
+            sum = numbers - sum;
+        }}
+    if(operation == "*"){
+        while(cin >> numbers){
+            sum = numbers * sum;
+        }}
+    if(operation == "/"){
+        while(cin >> numbers){
+            sum = numbers / sum;
+        }}
+    cout << sum;
+}
